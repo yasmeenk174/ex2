02e4caf170a8d9182881b6915ff096c51df03cdb
Yasmeen Kadah <k.yasmeen@campus.technion.ac.il>
2022-05-05 17:23:20 +0300

    h

--- Player.h
+++ Player.h
@@ -2,17 +2,35 @@
 #define EX2_Player_H
 
 #include <string>
-#include "Player.h"
 #include "utilities.h"
+using namespace std;
 
 class Player
 {
     public:
+    Player(const string name, int Max_HP=100,  int force=5);
+    Player(const Player& Player);
+    ~Player();
+    Player& operator=(const string&);
+    void printInfo();
+    void levelUp();
+    int getLevel();
+    void buff(const int additionalForce&);
+    void heal(const int additionalHP&);
+    void damage(const int damage&);
+    bool isKnockedOut();
+    void addcoins(const int additionalCoins&);
+    bool pay(const int price&);
+    int getAttackStrength();
 
-
-
-
-    private:
+ 
+   private:
+   string m_name;
+   int m_level;
+   int m_force;
+   const int m_maxHP;
+   int m_HP;
+   unsigned int m_coins;
 
 };
 
