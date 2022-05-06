7ab0cc7eddc624eb0af846ca448ea360ef50798b
Donia Salama <donia.salama@campus.technion.ac.il>
2022-05-07 00:25:27 +0300

    mtm

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -8,11 +8,12 @@ GameStatus m_status;
 int currentCard;
 
 Mtmchkin::Mtmchkin(const char *playerName, const Card *cardArray, int numOfCards):
-     m_playerName(string(playerName)),m_numOfCards(numOfCards),m_status(GameStatus::MidGame),currentCard(0)
+     m_playerName(string(playerName)),m_cards(new Card[numOfCards]),m_numOfCards(numOfCards),m_status(GameStatus::MidGame),currentCard(0)
 {
     for (int i = 0; i < numOfCards; i++)
     {
         m_cards[i]=cardArray[i];
     }
 }
+
 
